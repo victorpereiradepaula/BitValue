70d2270926304bdc251e1346c4ea08a1194a82a3
Victor Pereira de Paula <victorpereiradepaula@gmail.com>
2019-12-04 14:34:17 -0400

    Initial Commit

---
+++ BitValue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitValue
+//
+//  Created by Victor Pereira de Paula on 04/12/19.
+//  Copyright © 2019 Victor Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
